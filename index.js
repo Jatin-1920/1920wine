dc48bf7465aa15541ef07ed72c3075a623c07abf
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-02 21:44:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,21 +10,17 @@ tablet: { smooth: true },
 
 locoScroll.on("scroll", ScrollTrigger.update);
 
-ScrollTrigger.scrollerProxy(".gsapPin", {
-  scrollTop(value) {
-    return arguments.length
-      ? locoScroll.scrollTo(value, 0, 0)
-      : locoScroll.scroll.instance.scroll.y;
-  },
+ScrollTrigger.scrollerProxy(".scrollContainer", {
+        scrollTop(value) {
+          return arguments.length ? locoScroll.scrollTo(value, 0, 0) :    locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
   getBoundingClientRect() {
-    return {
-      top: 0,
-      left: 0,
-      width: window.innerWidth,
-      height: window.innerHeight
-    };
-  }
-})
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".scrollContainer").style.transform ? "transform" : "fixed"
+});
+
 
 const prlxSection = document.querySelectorAll(".prlx-section")
 
@@ -46,6 +42,7 @@ prlxSection.forEach(e=>{
     ease:"none"
   })
 })
+
 
 let currentScroll = 0
 let isScrolling  = true
