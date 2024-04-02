8be6e67acf24f185ba9a53beeb2d5314cf289a4d
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-02 22:34:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ gsap.registerPlugin(ScrollTrigger)
 
     const lenis = new Lenis({
   duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-  direction: 'both', // vertical, horizontal
-  gestureDirection: 'both', // vertical, horizontal, both
-  smooth: true,
-  mouseMultiplier: 1,
-  smoothTouch: false,
-  touchMultiplier: 2,
-  infinite: false,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: 'vertical',
+    gestureDirection: 'vertical',
+    lerp: 0.05,
+    smooth: 2,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    wheelMultiplier: 1,
+    infinite: false,
+    autoResize: true,
 })
   
 lenis.on('scroll', ScrollTrigger.update)
