a405dd7062e352fcef78e535ee13552c89999d95
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-02 22:23:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 gsap.registerPlugin(ScrollTrigger)
 
-const locoScroll = new LocomotiveScroll({
-el: document.querySelector(".root"),
-smooth: true,
-smartphone: { smooth: true },
-tablet: { smooth: true },
-lerp:0.05,
-wheelMultiplier:1.2
-});
+const lenis = new Lenis({
+    smooth:true,
+        lerp:0.05
+  })
+lenis.on('scroll', ScrollTrigger.update)
 
-locoScroll.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
 
-ScrollTrigger.scrollerProxy(".scrollContainer", {
-        scrollTop(value) {
-          return arguments.length ? locoScroll.scrollTo(value, 0, 0) :    locoScroll.scroll.instance.scroll.y;
-  }, 
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".root").style.transform ? "transform" : "fixed"
-});
-
-
+gsap.ticker.lagSmoothing(0)
 const prlxSection = document.querySelectorAll(".prlx-section")
 
 prlxSection.forEach(e=>{
@@ -62,6 +50,4 @@ window.addEventListener("scroll",() => {
 
 
 
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-ScrollTrigger.refresh();
+  
