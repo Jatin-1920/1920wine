99ce39b8910557f999cd1368db88cf718efecc93
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-02 15:19:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger)
 
-const lenis = new Lenis({
+/*const lenis = new Lenis({
   duration: 1.5,
     direction: 'both',
     gestureDirection: 'both',
@@ -20,6 +20,55 @@ gsap.ticker.add((time)=>{
 })
 
 gsap.ticker.lagSmoothing(0)
+*/
+const body = document.body;
+const main = document.querySelector('.root');
+
+let sx = 0, // For scroll positions
+    sy = 0;
+let dx = sx, // For container positions
+    dy = sy;
+
+
+body.style.height = main.clientHeight + 'px';
+
+
+main.style.position = 'fixed';
+main.style.top = 0;
+main.style.left = 0;
+
+// Bind a scroll function
+window.addEventListener('scroll', easeScroll);
+
+
+function easeScroll() {
+  
+  sx = window.pageXOffset;
+  sy = window.pageYOffset;
+}
+
+
+window.requestAnimationFrame(render);
+
+function render(){
+  //We calculate our container position by linear interpolation method
+  dx = li(dx,sx,0.125);
+  dy = li(dy,sy,0.125);
+  
+  dx = Math.floor(dx * 100) / 100;
+  dy = Math.floor(dy * 100) / 100;
+  
+  
+  main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+ 
+  
+  
+  window.requestAnimationFrame(render);
+}
+
+function li(a, b, n) {
+  return (1 - n) * a + n * b;
+}
 
 const prlxSection = document.querySelectorAll(".prlx-section")
 
