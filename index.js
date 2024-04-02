fd05bcc1b574691de61030540a912a0f12b9a7a9
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-02 11:48:01 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+gsap.registerPlugin(ScrollTrigger)
+
+const prlxSection = document.querySelectorAll(".prlx-section")
+
+prlxSection.forEach(e=>{
+  const prlxImg  = e.querySelector(".prlx-item")
+  
+})
