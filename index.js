da3b81a8e4439cc068cf1dc213182cc1ad3b98eb
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-02 11:55:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ const prlxSection = document.querySelectorAll(".prlx-section")
 
 prlxSection.forEach(e=>{
   const prlxImg  = e.querySelector(".prlx-item")
-  
+  const prlxTl = gsap.timeline({
+    scrollTrigger: {
+        trigger:e,
+        start: "clamp(top bottom)",
+        end: "clamp(bottom top)",
+        scrub: true,
+      
+    }
+})
+  const yHeight = prlxImg.offsetHeight - e.offsetHeight 
+
+  prlxTl.fromTo(prlxImg,{y:-yHeight},{
+    y:0,
+    ease:"none"
+  })
 })
