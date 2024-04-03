32955e84df6ad2ea500c65c5d3b617d40e9aec29
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-03 16:35:57 +0530

    Update gsap.js

--- gsap.js
+++ gsap.js
@@ -9,7 +9,7 @@ const navTl = gsap.timeline({paused:true})
 navTl.to(".nav",{height:"100dvh",ease:"power2.inOut",
                  duration:.85}).from(".topNavLine",{scaleY:0,transformOrigin:"0% 0%" ,duration:1,
               ease:"power2.inOut"},0.2).from(".bottomNavLine",{scaleX:0,transformOrigin:"0% 0%" ,duration:1,
-            ease:"power2.inOut"},0.2).to(".nav-link",{opacity:1,y:0,duration:.7,
+            ease:"power2.inOut"},0.2).to(".nav-link",{opacity:1,duration:.7,stagger:.2
             ease:"power2.inOut"},0.56).from(".bottomLink i",{
   opacity:0,y:-50,duration:.7,stagger:.1,ease:"power2.inOut"
             },0.4)
