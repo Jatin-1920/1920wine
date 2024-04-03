0696830381a0efb9942a8785419e0d548615ce1a
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-03 11:25:46 +0530

    Update gsap.js

--- gsap.js
+++ gsap.js
@@ -8,7 +8,7 @@ const navTl = gsap.timeline({paused:true})
 
 navTl.to(".nav",{height:"0",ease:"power2.inOut",
                  duration:.85}).from(".topNavLine",{height:"0%", duration:.75,ease:"power2.inOut"},.5)
-const state = false
+let state = false
 
 function switchMenu() {
   state = !state
