f3630d85abc46ffc9dfb1eb823e94bb4b67d3154
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-03 11:39:29 +0530

    Update gsap.js

--- gsap.js
+++ gsap.js
@@ -7,7 +7,7 @@ const navToggle = document.querySelector(".menuToggle")
 const navTl = gsap.timeline({paused:true})
 
 navTl.to(".nav",{height:"100dvh",ease:"power2.inOut",
-                 duration:.85}).from(".topNavLine",{scale:0,transformOrigin:"0% 0%" ,duration:.75,ease:"power2.inOut"},.5)
+                 duration:.85}).from(".topNavLine",{scale:0,transformOrigin:"0% 0%" ,duration:1,ease:"power2.inOut"},.5)
 let state = false
 
 function switchMenu() {
