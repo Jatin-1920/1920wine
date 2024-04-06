983be1f93a99abb4ace82ef34cd1fea3e2ac3f24
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-06 22:01:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const cliplax = gsap.timeline({
 })
 
      cliplax.to(".clip1",{
-        clipPath: "inset(0 0 0 0)",
+        clipPath: polygon(100% 0, 0 0, 0 100%, 100% 100%);,
         ease:"none"
      })
 })
