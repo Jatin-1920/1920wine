26965e3ad1e56f88327afbed07437a10f030740d
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-07 13:41:53 +0530

    Update about.js

--- about.js
+++ about.js
@@ -1,1 +1,23 @@
+const races = document.querySelector(".horizontal-scroll")
+function getScrollAmount(){
+    let racesWidth = races.clientWidth
+    return -(racesWidth-window.innerWidth)
 
+}
+const slider = gsap.to(races,{
+    x:getScrollAmount,
+    duration:5,ease:"none",
+  
+
+})
+
+ScrollTrigger.create({
+    trigger:".horizontal-container",
+    start:"top 20%",
+    end:()=> `+=${getScrollAmount()*-1}`,
+    pin:true,
+    scrub:1,
+    invalidateOnRefresh:true,
+    animation:slider,
+  
+})
