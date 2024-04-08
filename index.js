1df8598e15c71df3ce9b31aa3efe57eef1f18ed7
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-08 21:06:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ function updateCartItem(item) {
 }
 
 
-function updating(item) {
+/*function updating(item) {
   let selectedItem = essenceItem.find(x=>x.class===item.class)
    if(selectedItem) {
       essenceItem.count += 1
@@ -230,7 +230,7 @@ function updating(item) {
    }
    
 }
-
+*/
 function addToCartItem(e) {
 const current = e.currentTarget.parentElement
    const oldprice = current.children[1].innerText
