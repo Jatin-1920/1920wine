eeceaf5c0d1d092f31028ed3c3a6aa614fb2a873
Jatin Yadav <129601211+Jatin-1920@users.noreply.github.com>
2024-04-14 20:53:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 const lenis = new Lenis({
-    lerp: 0.07,
+    lerp: 0.01,
     wheelMultiplier: 5, // scrolling speed for mouse wheel
   touchMultiplier: 0.7, // scrolling speed for touch events
   smoothWheel: true, // smooth scrolling for while events
